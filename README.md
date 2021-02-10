c5c9769e7903e8b91f41b3513cbc0a0329aaf119
matt <philmattdev@gmail.com>
2021-02-10 14:58:02 +0800

    Added some few more necessary files for pypi

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jarvis API
+Just a test release, no one should use it cause no one knows what it is.
--- setup.py
+++ setup.py
@@ -6,5 +6,20 @@ setup(
     version='0.1',
     license='MIT',
     description='This is the api used for a small WIP project.',
-    author='bossauh'
+    author='bossauh',
+    author_eemail='philmattdev@gmail.com',
+    url='https://github.com/bossauh/jarvisapi',
+    download_url='https://github.com/bossauh/jarvisapi/archive/v0.1-beta.tar.gz',
+    keywords=['jarvis', 'voice assistant'],
+    instal_requires=[
+        'requests',
+        'nltk'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
 )
